424f6bf4c61538cb324dfc31378e9096457be7c7
root <hairviction@gmail.com>
2025-02-12 16:27:41 +0200

    sg scanner

--- security_group_port_scanner/scanner.py
+++ security_group_port_scanner/scanner.py
@@ -16,23 +16,8 @@
 #     print(f'Outbound Rules: {sg["IpPermissionsEgress"]}')
 #     print('---')
 
-
-
-# import boto3
-# client = boto3.client('ec2')
-
-# security_groups = client.describe_security_groups()['SecurityGroups']
-# open_ports = []
-
-# for sg in security_groups:
-#     sg_id = sg['GroupId']
-#     sg_name = sg.get("GroupName", "Unknown Name")  # Handle missing name
-#     for ingress in sg['IpPermissions']:
-#         for ip_range in ingress['IpRanges']:
-#             if ip_range['CidrIp'] == '0.0.0.0/0':
-#                 open_ports.append(f"security group {sg_name} {sg_id} allows all ingress traffic")
-
-# print(open_ports)
+# documentation
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/client/describe_security_groups.html#
 
 import boto3
 client = boto3.client('ec2')
@@ -56,4 +41,4 @@ for sg in security_groups:
 if open_ports:
     print("\n".join(open_ports))
 else:
-    print("✅ No open ports found.")
+    print("No open ports found.")
